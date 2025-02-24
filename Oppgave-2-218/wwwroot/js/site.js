7f1cd0026c4b4f38cee2958ec2e9a8ade40597d4
Kreline1993 <n.engemann1993@gmail.com>
2025-02-24 09:36:27 +0100

    Created .Net MVC project Added map

--- Oppgave-2-218/wwwroot/js/site.js
+++ Oppgave-2-218/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var map = L.map('map').setView([58.1633, 8.0025], 16);
+
+
+L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png', {
+    maxZoom: 19,
+    attribution: '&copy; Kartverket</a>'
+}).addTo(map);
