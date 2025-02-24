503790354bb6b9ca9c04936382fec5e90642e0c7
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2025-02-24 12:26:40 +0100

    Added multiple datasets

--- Oppgave-2-218/wwwroot/js/site.js
+++ Oppgave-2-218/wwwroot/js/site.js
@@ -4,8 +4,34 @@
 // Write your JavaScript code.
 var map = L.map('map').setView([58.1633, 8.0025], 16);
 
-
+//Kartverket
 L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png', {
     maxZoom: 19,
     attribution: '&copy; Kartverket</a>'
+}).addTo(map);
+
+// Tilelayer for wind resources
+L.tileLayer.wms("https://nve.geodataonline.no/arcgis/services/Vindressurser/MapServer/WMSServer", {
+    // Replace '0' with the actual layer name as defined in the GetCapabilities document
+    layers: "Gj.snittlig_vindstyrke_120m_over_bakkeniva",
+    format: "image/png",
+    transparent: true,
+    attribution: "NVE Geodata Online",
+    opacity: 1
+}).addTo(map);
+
+// Add the WMS layer for terrain complexity
+L.tileLayer.wms("https://nve.geodataonline.no/arcgis/services/Vindressurser/MapServer/WMSServer", {
+    // Replace '0' with the actual layer name as defined in the GetCapabilities document
+    layers: "Terrengkompleksitet_RIX",
+    format: "image/png",
+    transparent: true,
+    attribution: "NVE Geodata Online",
+    opacity: 0
+}).addTo(map);
+
+//Google labels
+L.tileLayer('https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}', {
+    maxZoom: 19,
+    attribution: '&copy; Google</a>'
 }).addTo(map);
