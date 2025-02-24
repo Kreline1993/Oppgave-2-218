e6bdd7924f6f70f96b344613716d3ffa162d295f
Kreline1993 <n.engemann1993@gmail.com>
2025-02-24 09:19:54 +0100

    Add project files.

---
+++ Oppgave-2-218/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oppgave_2_218.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
